4d3089815d0e98b3fbdfcfbbc78b7f92ad0889a6
Nikita Vanychenko <postamentbox@bk.ru>
2021-11-14 23:58:51 +0300

    secant and half_divide

--- nonlineareq-find0/main.py
+++ nonlineareq-find0/main.py
@@ -1,41 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import math
 import numpy
 import matplotlib.pyplot as plt
 
 e = 1e-10
 
-def func(i):
-    return math.cos(i)
+# бисекция и секущей
+# ищет только 1 ноль на отрезке, больше (?)
+
+
+def func(arg):
+    return math.cos(arg)
+
 
 def half_divide_method(a, b, f):
     x = (a + b) / 2
-    if math.fabs(f(x)) >= e:
-        # print(x)
-        # x = (a + b) / 2
-        if f(a) * f(x) < 0:
-            half_divide_method(a,x,f)
-        if f(x) * f(b) < 0:
-            half_divide_method(x,b,f)
-    else:
-        print("0 in ", (a + b) / 2)
+    it = 0
+    while math.fabs(f(x)) >= e:
+        x = (a + b) / 2
+        a, b = (a, x) if f(a) * f(x) < 0 else (x, b)
+        it += 1
+    print("0 in ", x, "find in ", it, "iteration")
 
-x = numpy.arange(-2.0, 4.0, 0.1)
-y = []
-start = -2.0
-end = 4.0
+
+def secant(f, x0, x1, tol=e, max_iterations=100):
+    # (!) problem with sign, if segment by x includes 0
+    fx0 = f(x0)
+    fx1 = f(x1)
+
+    while abs(fx1) >= tol and max_iterations != 0:
+        x2 = (x0 * fx1 - x1 * fx0) / (fx1 - fx0)
+        x0, x1 = x1, x2
+        fx0, fx1 = fx1, f(x2)
+        max_iterations -= 1
+    print("0 in ", x1, "find in ", 100 - max_iterations, "iteration")
+
+
+start = 2.0
+end = 6.0
 step = 0.1
+x = numpy.arange(start, end, step)
+y = []
 for i in x:
     y.append(func(i))
 half_divide_method(start, end, func)
-plt.plot(x, y, c="black", label="Интерполируемая функция")
+secant(func, start, end)
+plt.plot(x, y, c="black")
 plt.show()
-
-# print ('root of the equation half_divide_method %s' % half_divide_method(a1, b1, func_glob))
-# print ('root of the equation newtons_method %s' % newtons_method(a1, b1, func_glob, func_first))
-
-# бисекция и секущей
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
